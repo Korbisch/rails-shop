739858b053f071f8362270e9db2ee6665f914726
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-11-05 17:21:28 +0100

    Add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(png|jpg|jpeg|gif)\z}i,
+    message: 'must be a png, jpg, jpeg or gif file'
+  }
 end
