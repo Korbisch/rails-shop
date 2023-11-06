32c0bf2ac4f22957c2725d32bc1e41c30a6f920a
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-11-06 19:24:04 +0100

    Create catalog controller tests

--- test/controllers/catalog_controller_test.rb
+++ test/controllers/catalog_controller_test.rb
@@ -4,5 +4,10 @@ class CatalogControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get catalog_index_url
     assert_response :success
+    assert_select 'nav a', minimum: 4
+    assert_select 'main ul li', 2
+    assert_select 'h2', 'Rails Test Product Title'
+    assert_select 'p', 'Test product description'
+    assert_select 'div', '$9.99'
   end
 end
