c6da68f779b9fb28522e659f302c3fb68c43a4d9
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-11-06 08:57:36 +0100

    Generate catalog controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
+  root 'catalog#index', as: 'catalog_index'
   resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
