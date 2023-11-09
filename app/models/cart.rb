2d08874194f0a1b751ce58fc1de7802084302234
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-11-09 14:37:04 +0100

    Add method to add line items to carts

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,14 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  # @param [Integer] product_id
+  def add_line_item(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.new(product_id: product_id)
+    end
+    current_item
+  end
 end
