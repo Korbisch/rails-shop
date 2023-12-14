5c84bca7ff4c2f978b5164ee49b4ce8c49e7297f
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-12-14 11:25:51 +0100

    Fix "Unpermitted parameters" console error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -86,11 +86,11 @@ class OrdersController < ApplicationController
 
   def pay_type_params
     if order_params[:pay_type] == "Credit card"
-      params.require(:order).permit(:credit_card_number, :expiration_date)
+      order_params.permit(:credit_card_number, :expiration_date)
     elsif order_params[:pay_type] == "Check"
-      params.require(:order).permit(:routing_number, :account_number)
+      order_params.permit(:routing_number, :account_number)
     elsif order_params[:pay_type] == "Purchase order"
-      params.require(:order).permit(:po_number)
+      order_params.permit(:po_number)
     else
       {}
     end
