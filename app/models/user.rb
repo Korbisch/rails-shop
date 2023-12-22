689a997d93b098493fe91f4e694f0129fc06ecc8
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2023-12-22 17:01:01 +0100

    Add sidebar and prohibit deletion of only admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
+  after_destroy :ensure_an_admin_remains
+
+  class AdminDeletionError < StandardError
+  end
+
+  private
+
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise AdminDeletionError.new "Can't delete the only user"
+    end
+  end
 end
